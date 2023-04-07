30d755c1fab72dc2487b07efcdcdeb697977eafa
Emilie <EmilieMontpre@outlook.com>
2023-04-07 15:36:39 +0200

    Image in the nav

--- pages/components/Nav/index.tsx
+++ pages/components/Nav/index.tsx
@@ -1,11 +1,12 @@
 import Reseaux from "../Reseaux";
 import Link from "next/link"
+import Image from "next/image"
 
 function Navigation() {
     return (
         <>
             <nav className="flex justify-between items-center px-1 py-4">
-                <Link href="/"><img src="/Logo.svg" /></Link>
+                <Link href="/"><Image width={120} height={120} alt="Logo" src="/Logo.svg"  /></Link>
                 <ul className="flex justify-between w-1/3">
                     <li className="flex items-center"><Link href="">Services</Link></li>
                     <li className="flex items-center"><Link href="#portfolio">Portfolios</Link></li>
@@ -14,7 +15,7 @@ function Navigation() {
                     <li className="flex items-center"><Link href="/api/auth/signin/credentials">Admin</Link></li>
                 </ul>
                 <div className="flex justify-between items-center">
-                    <Link href="/"><img src="/moon.svg" /></Link>
+                    <Link href="/"><Image width={30} height={30} alt="Icon for darkMode" src="/moon.svg" /></Link>
                     <Link href="/CV.pdf" className="px-2.5 mx-12 text-sm">Resume</Link>
                     <Link href="/contact" className="px-6 py-4 border border-MyColor rounded-lg text-sm">Contact Me</Link>
                 </div>
