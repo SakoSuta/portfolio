8703144985ca9082495768badd96752033b542ef
Emilie <EmilieMontpre@outlook.com>
2023-04-13 17:52:26 +0200

    avancement contact

--- pages/contact/contact-component/index.tsx
+++ pages/contact/contact-component/index.tsx
@@ -3,12 +3,15 @@ import { FC } from 'react';
 const ContactForm: FC = () => {
     return (
         <>
-            <form>
-                <input type="text" id="name" name="name" />
-                <input type="email" id="email" name="email" />
-                <textarea id="message" name="message" />
-                <button type="submit">Send Message</button>
-            </form>
+            <div className='w-full px-6'>
+                <h2 className='my-4 font-medium text-xl'>Need more informations about me or else ?</h2>
+                <form>
+                    <input className='my-2 p-2 border-solid border-2 rounded-lg border-MyColor' type="text" id="name" name="name" placeholder='Name'/><br />
+                    <input className='my-2 p-2 border-solid border-2 rounded-lg border-MyColor' type="email" id="email" name="email" placeholder='Email'/><br />
+                    <textarea className='my-2 p-2 border-solid border-2 rounded-lg border-MyColor' id="message" name="message" placeholder='Message'/><br />
+                    <button type="submit">Send Message</button>
+                </form>
+            </div>
         </>
     )
 }
